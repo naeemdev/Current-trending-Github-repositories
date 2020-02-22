5bd2a7de446c4124b30bea033c21c80fc5ed8445
Muhammad Naeem <naeem0313@gmail.com>
2020-02-22 19:25:59 +0500

    Pull-to-refresh to fetch fresh data Pull-to-refresh to fetch  in java class

--- app/src/main/java/com/gojek_assignment_android/MainActivity.kt
+++ app/src/main/java/com/gojek_assignment_android/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.gojek_assignment_android
 
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     var mShimmerViewContainer: ShimmerFrameLayout? = null
     var layout_nointernet: LinearLayout? = null
     var layout_shimmer: LinearLayout? = null
+    private var mSwipeRefreshLayout: SwipeRefreshLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +28,16 @@ class MainActivity : AppCompatActivity() {
 
         /*Find by id shimmer  layout for visibillty*/
         layout_shimmer = findViewById(R.id.layout_shimmer)
+        mSwipeRefreshLayout = findViewById(R.id.mSwipeRefreshLayout)
+
+        mSwipeRefreshLayout!!.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+
+            /// call api for more images
+            layout_shimmer!!.visibility = View.VISIBLE
+            mSwipeRefreshLayout!!.isRefreshing = false
+
+
+        })
     }
 
     override fun onResume() {
