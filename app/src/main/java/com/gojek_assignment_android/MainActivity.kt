bd93a426dfdf0d9c6883bd5c8dcf2ebf0d1f2cfd
Muhammad Naeem <naeem0313@gmail.com>
2020-02-22 19:12:40 +0500

    find by ID Nointernet layout in main activity (Java class) for visibilty by default set  visibility gone
    find by ID Shimmer layout in main activity (Java class) for visibilty  by default set  visibility Visible

--- app/src/main/java/com/gojek_assignment_android/MainActivity.kt
+++ app/src/main/java/com/gojek_assignment_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gojek_assignment_android
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,14 +9,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var mShimmerViewContainer: ShimmerFrameLayout? = null
+    var layout_nointernet: LinearLayout? = null
+    var layout_shimmer: LinearLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
 
-        //findid the shimmer View from xml
+        //find by id  shimmer View from xml
         mShimmerViewContainer = findViewById(R.id.shimmer_view_container)
+
+        /*Find by id  No internet  layout for visibillty*/
+        layout_nointernet = findViewById(R.id.layout_nointernet)
+
+        /*Find by id shimmer  layout for visibillty*/
+        layout_shimmer = findViewById(R.id.layout_shimmer)
     }
 
     override fun onResume() {
