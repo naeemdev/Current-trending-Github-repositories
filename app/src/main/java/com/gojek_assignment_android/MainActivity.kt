a3b1527d26252974a7498698341a1403c4d11e48
Muhammad Naeem <naeem0313@gmail.com>
2020-02-24 11:54:21 +0500

    Store and get List from share pref  with validation

--- app/src/main/java/com/gojek_assignment_android/MainActivity.kt
+++ app/src/main/java/com/gojek_assignment_android/MainActivity.kt
@@ -14,10 +14,12 @@ import com.facebook.shimmer.ShimmerFrameLayout
 import com.gojek_assignment_android.CustomAdatper.TrendingRepo_CustomAdapter
 import com.gojek_assignment_android.interfaces.ResponseListener
 import com.gojek_assignment_android.model.TrendingRepositories_model
+import com.gojek_assignment_android.sharedprefer.SharedPreferenceHelper
 import com.gojek_assignment_android.viewmodel.TrendingRepositories_Viewmodel
 import com.hendraanggrian.recyclerview.widget.ExpandableRecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 import com.gojek_assignment_android.Utils.isConnectedToNetwork as isConnectedToNetwork1
 
 class MainActivity : AppCompatActivity(), ResponseListener {
@@ -98,14 +100,19 @@ class MainActivity : AppCompatActivity(), ResponseListener {
             layout_nointernet!!.visibility = View.GONE
             mTrendingrepoLiveData.clear()
 
-            mTrendingRepositories_Viewmodel.getTrendingRepository(this)!!.observe(this,
-                Observer<List<TrendingRepositories_model>> { mTrendingRepositories_model ->
-                    Log.e("sizea_rray", mTrendingRepositories_model.size.toString())
-                    mShimmerViewContainer!!.stopShimmerAnimation()
-                    layout_shimmer!!.visibility = View.GONE
-                    mTrendingrepoLiveData.addAll(mTrendingRepositories_model)
-                    setdatatorecyclerView()
-                })
+            mTrendingrepoLiveData =
+                mSharedPreferenceHelper!!.getTrendingRepo() as MutableList<TrendingRepositories_model>
+
+            if (mSharedPreferenceHelper!!.expired > System.currentTimeMillis()) {
+
+                mShimmerViewContainer!!.stopShimmerAnimation()
+                layout_shimmer!!.visibility = View.GONE
+
+                setdatatorecyclerView()
+            } else {
+
+                calltoapi()
+            }
 
         } else {
             layout_shimmer!!.visibility = View.GONE
@@ -114,14 +121,35 @@ class MainActivity : AppCompatActivity(), ResponseListener {
     }
 
 
+    fun calltoapi() {
+        mSharedPreferenceHelper!!.clearPrefs()
+        mTrendingRepositories_Viewmodel.getTrendingRepository(this)!!.observe(this,
+            Observer<List<TrendingRepositories_model>> { mTrendingRepositories_model ->
+                Log.e("sizea_rray", mTrendingRepositories_model.size.toString())
+                mShimmerViewContainer!!.stopShimmerAnimation()
+                layout_shimmer!!.visibility = View.GONE
+                mTrendingrepoLiveData.addAll(mTrendingRepositories_model)
+                mSharedPreferenceHelper!!.setTrendingRepoData(mTrendingRepositories_model)
+                mSharedPreferenceHelper!!.expired =
+                    System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(120)
+                setdatatorecyclerView()
+            })
+    }
     fun setdatatorecyclerView() {
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView!!.layoutManager = layoutManager
-        mTrendingRepo_CustomAdapter =
-            TrendingRepo_CustomAdapter(layoutManager, this, mTrendingrepoLiveData)
-        //set the CustomAdapter
-        recyclerView!!.setAdapter(mTrendingRepo_CustomAdapter)
 
+        if (mTrendingrepoLiveData.size > 0) {
+            layout_shimmer!!.visibility = View.GONE
+            layout_nointernet!!.visibility = View.GONE
+            val layoutManager = LinearLayoutManager(this)
+            recyclerView!!.layoutManager = layoutManager
+            mTrendingRepo_CustomAdapter =
+                TrendingRepo_CustomAdapter(layoutManager, this, mTrendingrepoLiveData)
+            //set the CustomAdapter
+            recyclerView!!.setAdapter(mTrendingRepo_CustomAdapter)
+        } else {
+            layout_shimmer!!.visibility = View.GONE
+            layout_nointernet!!.visibility = View.VISIBLE
+        }
 
     }
     ///click listener for Retry button
