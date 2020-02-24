19ea5c922e5040dc43e6018d1fb5f1192adb78c3
Muhammad Naeem <naeem0313@gmail.com>
2020-02-24 11:49:26 +0500

    Create expire time funcation

--- app/src/main/java/com/gojek_assignment_android/sharedprefer/SharedPreferenceHelper.kt
+++ app/src/main/java/com/gojek_assignment_android/sharedprefer/SharedPreferenceHelper.kt
@@ -1,9 +1,45 @@
 package com.gojek_assignment_android.sharedprefer
 
 import android.content.Context
+import android.content.SharedPreferences
  
+import java.util.concurrent.TimeUnit
+
 
 class SharedPreferenceHelper(context: Context) {
+
+    private val preferences: SharedPreferences
+    private var editor: SharedPreferences.Editor? = null
+
+    private val REPO_DATA_KEY = "reporp_data"
+    private val EXPIRE_DATA_KEY = "ExpiredDat"
+
+
+    //for expired
+    var expired: Long
+        get() = preferences.getLong(
+            EXPIRE_DATA_KEY,
+            System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(0)
+        )
+        set(expiredda) {
+            editor = preferences.edit()
+            editor!!.putLong(EXPIRE_DATA_KEY, expiredda!!)
+            editor!!.apply()
+            editor!!.commit()
+        }
+
+
+    fun clearPrefs() {
+        preferences.edit().clear().commit()
+    }
+
+
+    init {
+        preferences =
+            context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+    }
+
+
 
 
 }
